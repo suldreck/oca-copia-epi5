76ad5898d7ac7690d43a15f7c4e95bc8fa76d343
suldreck <suldreck@hotmail.com>
2018-05-31 21:38:06 +0200

    guardado
    todo bien

---
+++ Assets/Scripts/DiceTotalDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceTotalDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        theStateManager = GameObject.FindObjectOfType<stateManager>();
+	}
+
+    stateManager theStateManager;
+	
+	// Update is called once per frame
+	void Update () {
+        if(theStateManager.IsDoneRolling == false)
+        {
+            GetComponent<Text>().text = "= ?";
+        }
+        else
+        {
+            GetComponent<Text>().text = "= " + theStateManager.DiceTotal;
+        }
+
+	}
+}
---
+++ Assets/stateManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stateManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public int NumberOfPlayers = 2;
+    public int CurrentPplayerID = 0;
+    public int DiceTotal;
+    public bool IsDoneRolling = false;
+    public bool IsDoneClicking = false;
+    public bool IsDoneAnimating = false;
+    // Update is called once per frame
+
+    public void NewTurn()
+    {
+        // This is the start of a player's turn.
+        // We don't have a roll for them yet.
+
+        IsDoneRolling   = false;//a dejrlo todo simetrico xDD
+        IsDoneClicking  = false;
+        IsDoneAnimating = false;
+        CurrentPplayerID=(CurrentPplayerID + 1) % NumberOfPlayers;
+    }
+    void Update()
+    {
+        if(IsDoneRolling && IsDoneClicking && IsDoneAnimating)
+        Debug.Log("Turn is done!");
+        NewTurn();
+
+    }
+}
